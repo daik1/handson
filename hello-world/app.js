67bc62202fa28be42724e62bc481bcd5088b54af
daikishinohara <shino124sd@gmail.com>
2021-05-12 23:47:08 +0900

    a

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,33 +1,33 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient({
+  region: "ap-northeast-1", //リージョン
+});
+exports.handler = async (event) => {
+  const sales = event.sales;
+  let params = {
+    TableName: myTableName, //テーブル名を指定
+    IndexName: myGSI, //インデックス名を指定
+    ExpressionAttributeNames: { "#Sales": "Sales" },
+    ExpressionAttributeValues: { ":val": sales },
+    KeyConditionExpression: "#Sales = :val", //上の２文はプレースホルダー
+  };
+  const request = await dynamo.query(params).promise();
+  //queryされた結果をsesでメールで送る
+  // const params = {
+  //   Destination: {
+  //     ToAddresses: [shino124sd@gmail.com],
+  //   },
+  //   Message: {
+  //     Body: {
+  //       Text: { Data: "Test" },
+  //     },
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  //     Subject: { Data: "Test Email" },
+  //   },
+  //   Source: "SourceEmailAddress",
+  // };
 
-    return response
+  // return ses.sendEmail(params).promise();
+
+  return request.Count;
 };

d0e0ed8c2f74bbbeacc8e6ca08647cd8b01c69e0
daikishinohara <shino124sd@gmail.com>
2021-05-14 09:43:07 +0900

    a

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,9 +1,11 @@
+import { DynamoDB } from "aws-sdk";
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient({
   region: "ap-northeast-1", //リージョン
 });
+const ses = new AWS.SES({ region: "us-east-1" });
 exports.lambdaHandler = async (event) => {
-  const sales = event.sales;
+  const sales = event.Sales;
   let params = {
     TableName: "myTableName", //テーブル名を指定
     IndexName: "myGSI", //インデックス名を指定
@@ -13,5 +15,25 @@ exports.lambdaHandler = async (event) => {
   };
   const request = await dynamo.query(params).promise();
 
-  return request.Count;
+  let mailItem = {
+    Destination: {
+      ToAddresses: ["shino124sd@gmail.com"],
+    },
+    Message: {
+      Body: { Text: { Data: request } },
+      Subject: { Data: "件名" },
+    },
+    Source: "検証済みの送信元アドレス",
+  };
+
+  // メール送信
+  ses.sendEmail(mailItem, function (err, data) {
+    if (err) {
+      console.log(err);
+      context.fail(err);
+    }
+    console.log(data);
+    context.succeed(data);
+  });
+  // return request.Count;
 };
